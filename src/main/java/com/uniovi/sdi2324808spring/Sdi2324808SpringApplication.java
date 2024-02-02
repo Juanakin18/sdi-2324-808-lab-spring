4418fb3a989b31d4aff6b9c21404c59bae4704e6
Juanakin18 <juaniteje@gmail.com>
2024-02-02 17:16:14 +0100

    Initial commit

---
+++ src/main/java/com/uniovi/sdi2324808spring/Sdi2324808SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324808spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324808SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324808SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniovi/sdi2324808spring/Sdi2324808SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324808spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324808SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
